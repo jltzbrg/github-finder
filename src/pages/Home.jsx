0e0778a55ad6db41ba05dcf8639c70b214ca998d
Julio Litzenberg <julio.litzenberg@gmail.com>
2022-02-27 22:57:22 +0100

    Create  User Search Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import UserResults from '../components/layout/users/UserResults'
+import UserSearch from '../components/layout/users/UserSearch'
 
 export default function Home() {
   return (
     <>
-      {/* SEARCH */}
+      <UserSearch />
       <UserResults />
     </>
   )
