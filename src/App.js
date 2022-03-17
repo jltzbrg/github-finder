46cbca27e69e3acf81e8fae1de3363e36a6678a2
Julio Litzenberg <julio.litzenberg@gmail.com>
2022-03-17 13:26:10 +0100

    Style the Aler component + fix bug with UserSearch

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import About from './pages/About'
-import Home from './pages/Home'
-import User from './pages/User'
-import NotFound from './pages/NotFound'
-import NavBar from './components/layout/NavBar'
-import Footer from './components/layout/Footer'
-import Alert from './components/layout/Alert'
-import { GithubProvider } from './context/Github/GithubContext'
-import { AlertProvider } from './context/alert/AlertContext'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import User from "./pages/User";
+import NotFound from "./pages/NotFound";
+import NavBar from "./components/layout/NavBar";
+import Footer from "./components/layout/Footer";
+import Alert from "./components/layout/Alert";
+import { GithubProvider } from "./context/Github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertContext";
 
 function App() {
   return (
     <GithubProvider>
       <AlertProvider>
         <Router>
-          <div className='flex flex-col justify-between h-screen'>
+          <div className="flex flex-col justify-between h-screen">
             <NavBar />
-            <main className='container px-3 pb-12 mx-auto'>
-              <Alert />
+            <main className="container px-3 pb-12 mx-auto">
               <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/about' element={<About />} />
-                <Route path='/user/:login' element={<User />} />
-                <Route path='/notfound' element={<NotFound />} />
-                <Route path='/*' element={<NotFound />} />
+                <Route
+                  path="/"
+                  element={
+                    <>
+                      <Alert />
+                      <Home />
+                    </>
+                  }
+                />
+                <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
+                <Route path="/notfound" element={<NotFound />} />
+                <Route path="/*" element={<NotFound />} />
               </Routes>
             </main>
             <Footer />
@@ -31,7 +38,7 @@ function App() {
         </Router>
       </AlertProvider>
     </GithubProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/layout/users/UserSearch.jsx
+++ src/components/layout/users/UserSearch.jsx
@@ -1,46 +1,46 @@
-import { useState, useContext } from 'react'
-import GithubContext from '../../../context/Github/GithubContext'
-import AlertContext from '../../../context/alert/AlertContext'
-import { searchUsers } from '../../../context/Github/GithubActions'
+import { useState, useContext } from "react";
+import GithubContext from "../../../context/Github/GithubContext";
+import AlertContext from "../../../context/alert/AlertContext";
+import { searchUsers } from "../../../context/Github/GithubActions";
 
 export default function UserSearch() {
-  const [text, setText] = useState('')
+  const [text, setText] = useState("");
 
-  const { users, dispatch } = useContext(GithubContext)
-  const { setAlert } = useContext(AlertContext)
+  const { users, dispatch } = useContext(GithubContext);
+  const { setAlert } = useContext(AlertContext);
 
-  const handleChange = (e) => {
-    setText(e.target.value)
-  }
+  const handleChange = (e) => setText(e.target.value);
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    if (text === '') {
-      setAlert('Please enter something', 'error')
+    e.preventDefault();
+
+    if (text === "") {
+      setAlert("Please enter something", "error");
     } else {
-      dispatch({ type: 'SET_LOADING' })
-      const users = searchUsers(text)
-      dispatch({ type: 'GET_USERS', payload: users })
-      setText('')
+      dispatch({ type: "SET_LOADING" });
+      const users = await searchUsers(text);
+      dispatch({ type: "GET_USERS", payload: users });
+      setText("");
     }
-  }
+  };
 
   return (
-    <div className='grid grid-cols-1 gap-8 mb-8 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2'>
+    <div className="grid grid-cols-1 gap-8 mb-8 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2">
       <div>
         <form onSubmit={handleSubmit}>
-          <div className='form-control'>
-            <div className='relative'>
+          <div className="form-control">
+            <div className="relative">
               <input
-                type='text'
-                className='w-full pr-40 text-black bg-gray-200 input input-lg'
-                placeholder='Search'
+                type="text"
+                className="w-full pr-40 text-black bg-gray-200 input input-lg"
+                placeholder="Search"
                 value={text}
                 onChange={handleChange}
               />
               <button
-                type='submit'
-                className='absolute top-0 right-0 rounded-l-none w-36 btn btn-lg'>
+                type="submit"
+                className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg"
+              >
                 Go
               </button>
             </div>
@@ -50,12 +50,13 @@ export default function UserSearch() {
       {users.length > 0 && (
         <div>
           <button
-            onClick={() => dispatch({ type: 'CLEAR_USERS' })}
-            className='btn btn-ghost btn-lg'>
+            onClick={() => dispatch({ type: "CLEAR_USERS" })}
+            className="btn btn-ghost btn-lg"
+          >
             Clear
           </button>
         </div>
       )}
     </div>
-  )
+  );
 }
